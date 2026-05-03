d82684dd0006557ff32498ace28af876bac732e9
MOHAMAD BUKHARI BIN AHMAD HUZAIRI <mohdbukhari03@gmail.com>
2026-05-03 13:31:35 +0800

    Fix: Force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. TAMBAH BARIS NI KAT ATAS
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. TAMBAH KOD NI SUPAYA DIA PAKSA GUNA HTTPS MASA LIVE
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
